03a7d364e11334afe88035364886e4d8125a5d6b
Nonpenguin <pendragon707@mail.ru>
2019-10-09 14:15:11 +0300

    Add L2

--- L2/Program.cs
+++ L2/Program.cs
@@ -12,41 +12,118 @@ namespace L2
         void Print();
     }
 
-    class Rectangle : IPrint, GeomFig
+    class Rectangle : GeomFig, IPrint
     {
         double widht;
         double hight;
-        Rectangle() { }
-        Rectangle(double w, double h) { widht = w; hight = h; }
-        public override string ToString();
-        public void IPrint();
+        public Rectangle() { }
+        public Rectangle(double w, double h) { widht = w; hight = h; }
+        public override string ToString()
+        {
+            return "Ширина: " + widht.ToString() + " Длина: " + hight.ToString() + " Площадь: " + (widht * hight).ToString();
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
     }
 
-    class Square : IPrint, GeomFig
+    class Square : GeomFig, IPrint
     {
         double side;
-        Square() { }
-        Square(double s) { side = s; }
-        public override string ToString();
-        public void IPrint();
+        public Square() { }
+        public Square(double s) { side = s; }
+        public override string ToString()
+        {
+
+            return "Длина стороны: " + side.ToString() + " Площадь: " + (side * side).ToString();
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
     }
 
-    class Circle : IPrint, GeomFig
+    class Circle : GeomFig, IPrint
     {
         double radius;
-        Circle() { }
-        Circle(double r) { radius = r; }
-        public override string ToString();
-        public void IPrint();
+        public Circle() { }
+        public Circle(double r) { radius = r; }
+        public override string ToString()
+        {
+            return "Радиус: " + radius.ToString() + " Площадь: " + (Math.Pow(radius, 2) * Math.PI).ToString();
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
     }
 
+    class Menu
+    {
+        int indicator;
+        bool success;
+        public int menu()
+        {
+            Console.WriteLine("1 - печать объекта Rectangle");
+            Console.WriteLine("2 - печать объекта Square");
+            Console.WriteLine("3 - печать объекта Circle");
+            Console.WriteLine("4 - выход");
+            Console.WriteLine("Введите номер действия");
+            success = Int32.TryParse(Console.ReadLine(), out indicator);
+            if (!success)
+            {
+                Console.WriteLine();
+                indicator = menu();
+            }
+            return indicator;
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Подопригорова Н. ИУ5-34Б";
+            Menu menu = new Menu();
+            Rectangle rectangle = new Rectangle(5, 20);
+            Square square = new Square(6);
+            Circle circle = new Circle(3);
+            while (true)
+            {
+                switch (menu.menu())
+                {
+                    case 1:
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Печать объекта Rectangle");
+                            rectangle.Print();
+                            Console.WriteLine();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Печать объекта Square");
+                            square.Print();
+                            Console.WriteLine();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Печать объекта Circle");
+                            circle.Print();
+                            Console.WriteLine();
+                            break;
+                        }
+                    case 4:
+                        {
+                            return;
+                        }
+                }
+            }
         }
     }
-}
 
+}
