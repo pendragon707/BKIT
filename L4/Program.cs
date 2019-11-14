3eb20e72f899e10d84754de98bde36b0b323c874
Nonpenguin <pendragon707@mail.ru>
2019-11-14 14:36:46 +0300

    add L4

---
+++ L4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ L_4/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace L_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.but_search = new System.Windows.Forms.Button();
+            this.text_search = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.result_search = new System.Windows.Forms.ListBox();
+            this.status = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // but_search
+            // 
+            this.but_search.Location = new System.Drawing.Point(676, 36);
+            this.but_search.Name = "but_search";
+            this.but_search.Size = new System.Drawing.Size(92, 26);
+            this.but_search.TabIndex = 0;
+            this.but_search.Text = "Поиск";
+            this.but_search.UseVisualStyleBackColor = true;
+            this.but_search.Click += new System.EventHandler(this.but_search_Click);
+            // 
+            // text_search
+            // 
+            this.text_search.Location = new System.Drawing.Point(34, 36);
+            this.text_search.Name = "text_search";
+            this.text_search.Size = new System.Drawing.Size(626, 26);
+            this.text_search.TabIndex = 1;
+            this.text_search.KeyDown += new System.Windows.Forms.KeyEventHandler(this.search_keydown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытиеToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(65, 29);
+            this.fileToolStripMenuItem.Text = "&Файл";
+            // 
+            // открытиеToolStripMenuItem
+            // 
+            this.открытиеToolStripMenuItem.Name = "открытиеToolStripMenuItem";
+            this.открытиеToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.открытиеToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.открытиеToolStripMenuItem.Text = "&Открыть";
+            this.открытиеToolStripMenuItem.Click += new System.EventHandler(this.on_open_click);
+            // 
+            // result_search
+            // 
+            this.result_search.ItemHeight = 20;
+            this.result_search.Location = new System.Drawing.Point(34, 86);
+            this.result_search.Name = "result_search";
+            this.result_search.Size = new System.Drawing.Size(734, 304);
+            this.result_search.TabIndex = 3;
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(34, 401);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(58, 20);
+            this.status.TabIndex = 4;
+            this.status.Text = "Время";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.result_search);
+            this.Controls.Add(this.text_search);
+            this.Controls.Add(this.but_search);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button but_search;
+        private System.Windows.Forms.TextBox text_search;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытиеToolStripMenuItem;
+        private System.Windows.Forms.ListBox result_search;
+        private System.Windows.Forms.Label status;
+    }
+}
+
